e7c59b1e07256064022506635ea1dca20e196e2f
shshrukh <shshrukh.arbaz@gmail.com>
2025-07-03 04:59:50 +0500

    feat: add genQuestions function to generate random math questions based on selected digit level

--- logic.js
+++ logic.js
@@ -3,6 +3,7 @@ const state = {
     questionDigits: 0,
     operations: ['+', '-'],
     question: null,
+    correctAswer : 0
 
 };
 
@@ -20,6 +21,9 @@ function cacheDOM() {
         gameLvl: document.querySelector('.choose-game-lvl'),
         gameLvlBtn: document.querySelectorAll('.game-lvl-btn'),
         displayQuestions: document.querySelector('.display-questions'),
+        inputQuestion : document.querySelector('.question-container'),
+        inputValue : document.querySelector('.answer-container'),
+        nextQuestionBtn: document.querySelector('.next-question-btn')
 
     })
 }
@@ -27,6 +31,7 @@ function cacheDOM() {
 function bindEvents() {
     els.nextBtn.addEventListener('click', handleNext);
     els.gameLvlBtn.forEach(element => element.addEventListener('click', handleSelectGameLvl));
+    els.nextQuestionBtn.addEventListener('click', handleNextQuestion)
 
 }
 
@@ -51,7 +56,12 @@ function handleSelectGameLvl(e) {
     els.displayQuestions.classList.remove('hidden')
 
 }
+function handleNextQuestion(){
+    const question = genQuestions(state.questionDigits);
+    state.question = question;
+    els.inputQuestion.value = `${question} = ?`;
 
+}
 function randomNum(digit) {
     if (digit === 1) return Math.floor(Math.random() * 9) + 1;
     if (digit === 2) return Math.floor(Math.random() * 90) + 10;
@@ -59,8 +69,8 @@ function randomNum(digit) {
 }
 
 function genQuestions(digit) {
-    const numOne = randomNum();
-    const numTwo = randomNum();
+    const numOne = randomNum(digit);
+    const numTwo = randomNum(digit);
     const operation = state.operations[Math.floor(Math.random() * state.operations.length)];
     return `${numOne} ${operation} ${numTwo}`;
 
