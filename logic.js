1f5b53c62e74cea1873237ad70682f1060f0acbb
shshrukh <shshrukh.arbaz@gmail.com>
2025-07-05 00:40:56 +0500

    feat: some small changes in all files

--- logic.js
+++ logic.js
@@ -3,7 +3,10 @@ const state = {
     questionDigits: 0,
     operations: ['+', '-'],
     question: null,
-    correctAswer : 0
+    correctAswer: 0,
+    totalQuestion: 10,
+    genQuestions: 0,
+    timer: 15
 
 };
 
@@ -21,9 +24,13 @@ function cacheDOM() {
         gameLvl: document.querySelector('.choose-game-lvl'),
         gameLvlBtn: document.querySelectorAll('.game-lvl-btn'),
         displayQuestions: document.querySelector('.display-questions'),
-        inputQuestion : document.querySelector('.question-container'),
-        inputValue : document.querySelector('.answer-container'),
-        nextQuestionBtn: document.querySelector('.next-question-btn')
+        inputQuestion: document.querySelector('.question-container'),
+        inputValue: document.querySelector('.input-number'),
+        nextQuestionBtn: document.querySelector('.submit-answer'),
+        result : document.querySelector('.final-score'),
+        scoreContainer : document.querySelector('.final-result'),
+        restartBtn :document.querySelector('.restart-game'),
+        
 
     })
 }
@@ -31,37 +38,47 @@ function cacheDOM() {
 function bindEvents() {
     els.nextBtn.addEventListener('click', handleNext);
     els.gameLvlBtn.forEach(element => element.addEventListener('click', handleSelectGameLvl));
-    els.nextQuestionBtn.addEventListener('click', handleNextQuestion)
+    els.nextQuestionBtn.addEventListener('click', handleNextQuestion);
+    els.restartBtn.addEventListener('click' , handleRestart)
 
 }
 
 function handleNext() {
-    els.gameGuide.classList.add('hidden')
-    els.gameLvl.classList.remove('hidden')
+    els.gameGuide.classList.add('hidden');
+    els.gameLvl.classList.remove('hidden');
 
 }
 function handleSelectGameLvl(e) {
-    if (e.target.innerText === 'One digit') {
-        state.questionDigits = 1;
-        console.log(state.questionDigits, typeof state.questionDigits);
-
-    } else if (e.target.innerText === 'Two digit') {
-        state.questionDigits = 2;
-        console.log(state.questionDigits, typeof state.questionDigits);
-    } else {
-        state.questionDigits = 3;
-        console.log(state.questionDigits, typeof state.questionDigits);
-    }
+    if (e.target.innerText === 'One digit') { state.questionDigits = 1 };
+    if (e.target.innerText === 'Two digit') { state.questionDigits = 2 };
+    if (e.target.innerText === 'Three digit') { state.questionDigits = 3 };
     els.gameLvl.classList.add('hidden');
     els.displayQuestions.classList.remove('hidden')
 
-}
-function handleNextQuestion(){
-    const question = genQuestions(state.questionDigits);
-    state.question = question;
-    els.inputQuestion.value = `${question} = ?`;
+    renderQuestion();
 
 }
+
+function handleNextQuestion() {
+    if (!els.inputValue.value) {
+        alert('please enter the value')
+        return undefined
+
+    }
+
+    const inputNumber = els.inputValue.value;
+    const correctAns = eval(state.question);
+    if (correctAns === Number(inputNumber)) {
+        state.correctAswer += 1;
+    }
+    console.log(state.correctAswer);
+
+    els.inputValue.value = '';
+    renderQuestion();
+
+
+}
+
 function randomNum(digit) {
     if (digit === 1) return Math.floor(Math.random() * 9) + 1;
     if (digit === 2) return Math.floor(Math.random() * 90) + 10;
@@ -69,18 +86,62 @@ function randomNum(digit) {
 }
 
 function genQuestions(digit) {
-    const numOne = randomNum(digit);
-    const numTwo = randomNum(digit);
+    let numOne = randomNum(digit);
+    let numTwo = randomNum(digit);
+
     const operation = state.operations[Math.floor(Math.random() * state.operations.length)];
+
+    while (operation === '-' && numOne < numTwo) {
+        numOne = randomNum(digit);
+        numTwo = randomNum(digit);
+    }
     return `${numOne} ${operation} ${numTwo}`;
 
 }
+function renderQuestion() {
 
+    state.genQuestions += 1;
+    console.log("question number", state.genQuestions);
+    if (state.genQuestions <= 10) {
+        const question = genQuestions(state.questionDigits);
+        state.question = question;
+        els.inputQuestion.value = `${question} = ?`;
+    }else{
+        finalScore()
+    }
+}
+function finalScore() {
+    persentage = state.correctAswer/10 *100
+    els.result.innerText = `your final score is ${persentage}%`
+    els.displayQuestions.classList.add('hidden');
+    els.scoreContainer.classList.remove('hidden');
+    
+}
 
-
-
-
-
+function handleRestart(){
+    state.questionDigits = 0;
+    state.question = null;
+    state.correctAswer = 0;
+    state.genQuestions = 0;
+    els.scoreContainer.classList.add('hidden')
+    handleNext();
+}
+function timer() {
+    const intervalId = setInterval(()=>{
+        state.timer -= 1;
+        console.log('interval tine', state.timer);
+        
+        if(state.timer === 0){
+            
+            console.log('inside timer', state.timer);
+            
+            clearInterval(intervalId);
+            state.timer = 15;
+        }
+    }
+    ,1500)
+}
+timer()
 
 
 document.addEventListener('DOMContentLoaded', init)
